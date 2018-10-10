2b9672b6c8f518cddabb04bc182d0f1936d335e2
Behnam Saeedi <saeedib@flip2.engr.oregonstate.edu>
2018-10-09 20:05:46 -0700

    modded install scrip

---
+++ Assignments/coding/install-user.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+pip install --upgrade --user pip
+pip install --user pandas
+pip install --user numpy
+pip install -U --user matplotlib
+pip install --user jupyter
--- Assignments/coding/install.sh
+++ Assignments/coding/install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+pip install --upgrade pip
 pip install pandas
 pip install numpy
+pip install -U matplotlib
 pip install jupyter
