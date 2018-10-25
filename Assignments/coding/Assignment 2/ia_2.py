b93e08685cd83bf7e867163d8d5c233f1bed57b2
Kamilla <kamilla.1201@gmail.com>
2018-10-25 01:48:58 -0700

    Added training and validation for kernel perceptron

--- Assignments/coding/Assignment 2/ia_2.py
+++ Assignments/coding/Assignment 2/ia_2.py
@@ -1,14 +1,21 @@
+import os
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
 	x_train, y_train = get_data("pa2_train.csv")
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-p", "--plot", action='store_true', help="if set, script will show plots")
+	args = parser.parse_args()
+
 	x_valid, y_valid = get_data("pa2_valid.csv")
 	x_test, _ = get_data("pa2_test_no_label.csv", test=True)
 
-	first_part(x_train, y_train, x_valid, y_valid, x_test)
-	second_part(x_train, y_train, x_valid, y_valid)
-	#third_part(x_train, y_train, x_valid, y_valid, x_test)
+	iters = 15
+	first_part(x_train, y_train, x_valid, y_valid, x_test, iters, args.plot)
+	second_part(x_train, y_train, x_valid, y_valid, iters, args.plot)
+	third_part(x_train, y_train, x_valid, y_valid, x_test, iters, args.plot)
 
 def get_data(filename, test=False):
 	x = np.genfromtxt(filename, delimiter=',', dtype=float)
@@ -23,109 +30,143 @@ def get_data(filename, test=False):
 		x = np.delete(x, [0], 1) # remove first column with y values
 	return x, y
 
-def first_part(x_train, y_train, x_valid, y_valid, x_test):
+def first_part(x_train, y_train, x_valid, y_valid, x_test, iters, plot):
 
 	# train
 	print "-------------------------------"
 	print "Part 1. Training"
 	print "-------------------------------"
-	all_weights, train_accuracies = online_perceptron(x_train, y_train, 15)
+	all_weights, train_accuracies = online_perceptron(x_train, y_train, iters)
 	#np.savetxt('my_out.txt', weights)
 
 	# validate
 	print "-------------------------------"
 	print "Part 1. Validation"
 	print "-------------------------------"
-	valid_accuracies = validate(all_weights, x_valid, y_valid)
+	valid_accuracies = perceptron_validate(all_weights, x_valid, y_valid)
 
 	# predict
 	print "-------------------------------"
 	print "Part 1. Prediction"
 	weights = all_weights[-2] # choose 14th iteration's weights
 	filename = 'oplabel.csv'
-	predictions = predict(x_test, weights)
+	predictions = perceptron_predict(x_test, weights)
 	np.savetxt(filename, predictions)
 	print "Saved to %s" % filename
 	print "-------------------------------"
 
-	plt.figure()
-	plt.plot(range(1, len(train_accuracies)+1), train_accuracies)
-	plt.plot(range(1, len(valid_accuracies)+1), valid_accuracies)
-	plt.show()
+	if plot:
+		plot_accuracies(train_accuracies, valid_accuracies)
 
-def second_part(x_train, y_train, x_valid, y_valid):
+def second_part(x_train, y_train, x_valid, y_valid, iters, plot):
 
 	# train
 	print "-------------------------------"
 	print "Part 2. Training"
 	print "-------------------------------"
-	all_weights, train_accuracies = average_perceptron(x_train, y_train, 15)
+	all_weights, train_accuracies = average_perceptron(x_train, y_train, iters)
 	#np.savetxt('my_out.txt', weights)
 
 	# validate
 	print "-------------------------------"
 	print "Part 2. Validation"
 	print "-------------------------------"
-	valid_accuracies = validate(all_weights, x_valid, y_valid)
+	valid_accuracies = perceptron_validate(all_weights, x_valid, y_valid)
 
-	plt.figure()
-	plt.plot(range(1, len(train_accuracies)+1), train_accuracies)
-	plt.plot(range(1, len(valid_accuracies)+1), valid_accuracies)
-	plt.show()
+	if plot:
+		plot_accuracies(train_accuracies, valid_accuracies)
 
-def third_part(x_train, y_train, x_valid, y_valid, x_test):
-	p = 1
-	# train
-	print "-------------------------------"
-	print "Part 3. Training"
-	print "-------------------------------"
-	alphas, train_accuracies = kernel_perceptron(x_train, y_train, p, 15)
-	#np.savetxt('my_out.txt', weights)
+def third_part(x_train, y_train, x_valid, y_valid, x_test, iters, plot):
+	all_p = [ 2, 3, 7, 15]
 
-	# validate
-	print "-------------------------------"
-	print "Part 3. Validation"
-	print "-------------------------------"
-	valid_accuracies = kernel_validate(x_valid, y_valid, p, alphas, 15,
-			x_train, y_train)
+	m = x_train.shape[0]
+	for p in all_p:
+		# compute kernel or upload existing from file, if exists
+		print "-------------------------------"
+		print "Part 3. Preparing kernel matrix for training with p=%d" % p
+		print "-------------------------------"
+		kernel = None
+		filename = 'kernels/kernel_train_p%d.csv' % p
+
+		if os.access(filename, os.R_OK):
+			print "Processing file %s" % filename
+			kernel = np.loadtxt(filename)
+		else:
+			print "File %s not found. Will create new one." % filename
+			kernel = compute_kernel(x_train, x_train, p)
+			np.savetxt(filename, kernel)
+
+		# train
+		print "-------------------------------"
+		print "Part 3. Training with p=%d" % p
+		print "-------------------------------"
+		alphas, train_accuracies = kernel_perceptron(kernel, y_train, iters)
+		#np.savetxt('my_out.txt', weights)
+
+		print "-------------------------------"
+		print "Part 3. Preparing kernel matrix for validation with p=%d" % p
+		print "-------------------------------"
+		kernel = None
+		filename = 'kernels/kernel_valid_p%d.csv' % p
+
+		if os.access(filename, os.R_OK):
+			print "Processing file %s" % filename
+			kernel = np.loadtxt(filename)
+		else:
+			print "File %s not found. Will create new one." % filename
+			kernel = compute_kernel(x_valid, x_train, p)
+			np.savetxt(filename, kernel)
+
+		# validate
+		print "-------------------------------"
+		print "Part 3. Validation with p=%d" % p
+		print "-------------------------------"
+		valid_accuracies = kernel_validate(kernel, y_valid, p, alphas, y_train)
+
+		if plot:
+			plot_accuracies(train_accuracies, valid_accuracies)
 
 	# predict
 	print "-------------------------------"
-	print "Part 1. Prediction"
-	#weights = all_weights[-2] # choose 14th iteration's weights
-	#filename = 'oplabel.csv'
-	#predictions = predict(x_test, weights)
+	print "Part 3. Prediction - TODO"
+	#predictions = 
 	#np.savetxt(filename, predictions)
 	#print "Saved to %s" % filename
 	print "-------------------------------"
 
+
+def plot_accuracies(train_accuracies, valid_accuracies):
 	plt.figure()
 	plt.plot(range(1, len(train_accuracies)+1), train_accuracies)
 	plt.plot(range(1, len(valid_accuracies)+1), valid_accuracies)
+	plt.gca().legend(('Training error','Validation error'))
 	plt.show()
-
 
 def kernel_function(x, y, p):
 	k = (1 + np.dot(x.T, y))**p
 	return k
 
-def kernel_perceptron(x, y, p, iters):
-	m = x.shape[0]
+def compute_kernel(x_1, x_2, p):
+	m = x_1.shape[0]
+	n = x_2.shape[0]
+	kernel = np.zeros((m, n))
+	for i in range(m):
+		for j in range(n):
+			kernel[i, j] = kernel_function(x_1[i, :], x_2[j, :], p)
+	return kernel
+
+def kernel_perceptron(kernel, y, iters):
+	m = kernel.shape[0]
 
 	alphas = []
 	accuracies = []
 
-	kernel = np.zeros((m, m))
-	for i in range(m):
-		for j in range(m):
-			kernel[i, j] = kernel_function(x[i, :], x[j, :], p)
-
+	alpha = np.zeros(m)
 	for iter_ in range(iters):
-		alpha = np.zeros(m)
 		correct_predictions = 0
 		for i in range(m):
 			u = np.dot(kernel[:, i], alpha * y)
-			if y[i]*u <= 0:
+			if y[i] * u <= 0:
 				alpha[i] += 1
 			else:
 				correct_predictions += 1
@@ -133,31 +174,32 @@ def kernel_perceptron(x, y, p, iters):
 		accuracy = correct_predictions / float(m)
 		print "Iteration %s, accuracy %s" % (iter_+1, accuracy)
 		accuracies.append(accuracy)
-		alphas.append(alpha)
+		alphas.append(alpha.copy())
 	return alphas, accuracies
 
-def kernel_validate(x, y, p, alphas, iters, x_train, y_train):
-	m = x.shape[0]
-
+def kernel_validate(kernel, y, p, alphas, y_train):
 	accuracies = []
-
-	kernel = np.zeros((m, m))
-	for i in range(x_train.shape[0]):
-		for j in range(m):
-			kernel[i, j] = kernel_function(x_train[i, :], x[j, :], p)
-
-	for iter_ in range(iters):
-		alpha = alphas[iter_]
-		correct_predictions = 0
-		for i in range(m):
-			u = np.dot(kernel[:, i], alpha * y_train)
-			if y[i]*u > 0:
-				correct_predictions += 1
-
-		accuracy = correct_predictions / float(m)
-		print "Iteration %s, accuracy %s" % (iter_+1, accuracy)
+	for i, alpha in enumerate(alphas):
+		predictions = kernel_predict(kernel, y_train, alpha, p)
+		u = predictions * y
+		correct_predictions = u[u > 0].shape[0]
+		accuracy = correct_predictions / float(y.shape[0])
+		print "Iteration %s, accuracy %s" % (i+1, accuracy)
 		accuracies.append(accuracy)
 	return accuracies
+
+def kernel_predict(kernel, y_train, alpha, p):
+	m = kernel.shape[0]
+	n = kernel.shape[1]
+	y_pred = np.zeros(m)
+	for i in range(m):
+		s = 0
+		for j in range(n):
+			if alpha[j] > 0:
+				s += alpha[j] * y_train[j] * kernel[i, j]
+
+		y_pred[i] = np.sign(s)
+	return y_pred
 
 def online_perceptron(x, y, iters):
 	all_weights = []
@@ -199,7 +241,7 @@ def average_perceptron(x, y, iters):
 			#loss += max(0, -1*u)
 			if u <= 0:
 				if s + c > 0:
-					avg_w = ((s * avg_w) + (c * weights)) / (s + c)
+					avg_w = ((s * avg_w) + (c * weights)) / float(s + c)
 				s = s + c
 				weights += y[i] * x[i]
 				c = 0
@@ -217,7 +259,7 @@ def average_perceptron(x, y, iters):
 		all_weights.append(avg_w.copy())
 	return all_weights, accuracies
 
-def validate(all_weights, x, y):
+def perceptron_validate(all_weights, x, y):
 	m = x.shape[0]
 	accuracies = []
 	for iter_, weights in enumerate(all_weights):
@@ -233,11 +275,10 @@ def validate(all_weights, x, y):
 		accuracies.append(accuracy)
 	return accuracies
 
-def predict(x, weights):
+def perceptron_predict(x, weights):
 	predictions = np.sign(np.dot(x, np.matrix(weights).T))
 
 	return predictions
-
 
 if __name__ == "__main__":
 	main()
