c1b9438b85e1e18f9ace443388bb39b4f9c8ddb1
BeNsAeI <behnam.saeidi@gmail.com>
2018-11-20 12:12:08 -0800

    Fixed the tree

--- Assignments/coding/Assignment 3/HW-3.py
+++ Assignments/coding/Assignment 3/HW-3.py
@@ -8,7 +8,8 @@ from collections import namedtuple
 # Example Declearation:
 ## myTree = Tree(0.5, 0x---, 0x---, 0x---)
 ## Or
-## myTree = Tree(Data1=0.5, Parent=0x---, Left=0x---, Right=0x---)
+## myTree = Tree(Data=0.5, Parent=0x---, Left=0x---, Right=0x---)
 Tree = namedtuple("Tree", "Data Parent Left Right")
 myTree = Tree(Data=0.5, Parent=None, Left=None, Right=None)
 print(type(myTree))
+print(myTree)
