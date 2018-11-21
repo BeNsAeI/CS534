080f20921f8d69779d12e72098bb31da95fdc6d0
BeNsAeI <behnam.saeidi@gmail.com>
2018-11-21 14:20:55 -0800

    Code initiated and imports the labels

--- Assignments/coding/Assignment 3/HW-3.py
+++ Assignments/coding/Assignment 3/HW-3.py
@@ -3,13 +3,53 @@ import argparse
 import numpy as np
 from collections import namedtuple
 
-
-# A convinient Tree structure
+path = "Sources/"
+train = "pa3_train_reduced"
+valid = "pa3_valid_reduced"
+format = ".csv"
+DEBUG = False
+VERBOSE = False
+# A c = Falseonvinient Tree structure
 # Example Declearation:
 ## myTree = Tree(0.5, 0x---, 0x---, 0x---)
 ## Or
 ## myTree = Tree(Data=0.5, Parent=0x---, Left=0x---, Right=0x---)
 Tree = namedtuple("Tree", "Data Parent Left Right")
-myTree = Tree(Data=0.5, Parent=None, Left=None, Right=None)
-print(type(myTree))
-print(myTree)
+
+def get_data(filename, test=False):
+	x = np.genfromtxt(filename, delimiter=',', dtype=float)
+	y = None
+	x = np.insert(x , [1], 1, axis=1)
+	if not test:
+		y = x[:, 0]
+		y[y == 5] = -1
+		y[y == 3] = 1
+		x = np.delete(x, [0], 1)
+	return x, y
+
+def d_print(value):
+	if VERBOSE:
+		print(value)
+
+def main():
+	#Parsing arguments:
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-p", "--plot", action='store_true', help="if set, script will show plots")
+	parser.add_argument("-d", "--debug", action='store_true', help="if set, debug mode is activated")
+	parser.add_argument("-v", "--verbose", action='store_true', help="if set, verbose mode is activated")
+	args = parser.parse_args()
+	global DEBUG
+	global VERBOSE
+	DEBUG = args.debug
+	VERBOSE = args.debug or args.verbose
+	# Getting the data:
+	d_print("Reading in Data...")
+	x_train, y_train = get_data(path+train+format, test=False)
+	d_print("Producing root data")
+	root_data = None
+	d_print("Initiating the Tree")
+	root = Tree(Data=root_data, Parent=None, Left=None, Right=None)
+	d_print("Root: "+str(root))
+
+if __name__ == "__main__":
+	main()
